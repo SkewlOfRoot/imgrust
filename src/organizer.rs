785ce4ce66fe951a66ce867939bcef22ea611e8b
SkewlOfRoot <giereck@hotmail.com>
2024-09-24 23:23:07 +0200

    The organize command will now also organize mp4 video files.

--- src/organizer.rs
+++ src/organizer.rs
@@ -1,7 +1,8 @@
-use anyhow::{anyhow, Ok};
+use anyhow::{anyhow, Context};
 use chrono::NaiveDateTime;
 use std::fs::{self};
 use std::path::{Path, PathBuf};
+use std::result::Result::Ok;
 
 pub fn organize_img_files(base_dir: &PathBuf) -> anyhow::Result<()> {
     if !base_dir.is_dir() {
@@ -14,64 +15,136 @@ pub fn organize_img_files(base_dir: &PathBuf) -> anyhow::Result<()> {
         let file_path = entry.path();
         let file_name = entry.file_name();
 
-        if is_image_file(&file_path) {
-            let date_taken = if let Some(date) = extract_image_date(&file_path) {
-                date
-            } else {
-                eprintln!("DateTime not found in EXIF data.");
-                continue;
-            };
+        let created_date = match get_media_file(&file_path) {
+            Some(m) => match m {
+                MediaFile::Image(img) => img.created_date(),
+                MediaFile::Video(vid) => vid.created_date(),
+            },
+            None => continue,
+        };
 
-            let new_dir_name = date_taken.date().format("%Y-%m").to_string();
+        let created_date = if let Some(date) = created_date {
+            date
+        } else {
+            eprintln!("DateTime not found in EXIF data.");
+            continue;
+        };
 
-            let new_dir_path = base_dir.join(new_dir_name);
-            if !new_dir_path.exists() {
-                fs::create_dir(&new_dir_path)?;
-            }
+        let new_dir_name = created_date.date().format("%Y-%m").to_string();
 
-            let new_file_path = new_dir_path.join(file_name);
+        let new_dir_path = base_dir.join(new_dir_name);
+        if !new_dir_path.exists() {
+            fs::create_dir(&new_dir_path)?;
+        }
 
-            if let Err(e) = fs::rename(&file_path, &new_file_path) {
-                eprintln!(
-                    "Failed to move file '{}' to new directory '{}': {}",
-                    file_path.display(),
-                    new_dir_path.display(),
-                    e
-                );
-            }
+        let new_file_path = new_dir_path.join(file_name);
+
+        if let Err(e) = fs::rename(&file_path, &new_file_path) {
+            eprintln!(
+                "Failed to move file '{}' to new directory '{}': {}",
+                file_path.display(),
+                new_dir_path.display(),
+                e
+            );
         }
     }
 
     Ok(())
 }
 
-fn is_image_file(file_path: &Path) -> bool {
+fn get_media_file(file_path: &Path) -> Option<MediaFile> {
     if file_path.is_dir() {
-        return false;
+        return None;
     }
 
-    return file_path.extension().unwrap_or_default() == "jpg"
-        || file_path.extension().unwrap_or_default() == "jpeg"
-        || file_path.extension().unwrap_or_default() == "png";
+    let extension = file_path
+        .extension()
+        .and_then(|ext| ext.to_str())
+        .unwrap_or_default();
+
+    match extension {
+        "jpg" | "jpeg" | "png" => Some(MediaFile::Image(Image::new(file_path))),
+        "mp4" => Some(MediaFile::Video(Video::new(file_path))),
+        _ => None,
+    }
 }
 
-fn extract_image_date(file_path: &Path) -> Option<NaiveDateTime> {
-    let exif_result = rexif::parse_file(file_path).unwrap();
+enum MediaFile {
+    Image(Image),
+    Video(Video),
+}
 
-    let date_time = exif_result
-        .entries
-        .iter()
-        .find(|t| t.tag == rexif::ExifTag::DateTime)
-        .map(|t| &t.value);
+struct Image {
+    file_path: PathBuf,
+}
+struct Video {
+    file_path: PathBuf,
+}
 
-    match date_time {
-        Some(date) => {
-            let d = &date.to_string();
-            Some(
-                NaiveDateTime::parse_from_str(d, "%Y:%m:%d %H:%M:%S")
-                    .expect("Error parsing string to date."),
-            )
+impl Image {
+    fn new(file_path: &Path) -> Image {
+        Image {
+            file_path: file_path.to_owned(),
         }
-        None => None,
+    }
+
+    fn created_date(&self) -> Option<NaiveDateTime> {
+        let exif_result = rexif::parse_file(&self.file_path).unwrap();
+
+        let date_time = exif_result
+            .entries
+            .iter()
+            .find(|t| t.tag == rexif::ExifTag::DateTime)
+            .map(|t| &t.value);
+
+        match date_time {
+            Some(date) => {
+                let d = &date.to_string();
+                Some(
+                    NaiveDateTime::parse_from_str(d, "%Y:%m:%d %H:%M:%S")
+                        .expect("Error parsing string to date."),
+                )
+            }
+            None => None,
+        }
+    }
+}
+
+impl Video {
+    fn new(file_path: &Path) -> Video {
+        Video {
+            file_path: file_path.to_owned(),
+        }
+    }
+
+    fn created_date(&self) -> Option<NaiveDateTime> {
+        let meta_data = ffprobe::ffprobe(&self.file_path)
+            .context("ffprobe")
+            .unwrap();
+
+        for stream in meta_data.streams {
+            let tags = if let Some(t) = stream.tags {
+                t
+            } else {
+                continue;
+            };
+
+            if let Some(date_str) = tags.creation_time {
+                let date = chrono::DateTime::parse_from_rfc3339(&date_str);
+                match date {
+                    Ok(d) => return Some(d.naive_utc()),
+                    Err(e) => {
+                        eprintln!(
+                            "Failed to parse '{}' to NativeDateTime. Error: {}",
+                            &date_str, e
+                        );
+                        continue;
+                    }
+                };
+            } else {
+                continue;
+            };
+        }
+        None
     }
 }
